99e11521d2c319f71a5a013252697a1d99c5ac9e
Manuela Mosandl <manuelamosandl@gmail.com>
2021-04-08 23:38:41 +0200

    add getName slot/form/rule function

--- actions/actions.py
+++ actions/actions.py
@@ -12,7 +12,8 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
-
+##from rasa_sdk.events import UserUtteranceReverted
+from rasa_sdk.forms import FormAction
 
 class ActionCompanySearch(Action):
 
@@ -28,3 +29,75 @@ class ActionCompanySearch(Action):
         dispatcher.utter_message(text="Die Adresse von {}: {}".format(company, address))
 
         return [SlotSet("address", address)]
+
+class locationForm(FormAction):
+    """Custom form action to fill all slots required to find specific type
+    of healthcare facilities in a certain city or zip code."""
+
+    def name(self) -> Text:
+        """Unique identifier of the form"""
+
+        return "company"
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        """A list of required slots that the form has to fill"""
+
+        return ["company", "location"]
+
+    def slot_mappings(self) -> Dict[Text, Any]:
+        return {"company": self.from_entity(entity="company",
+                                                  intent=["inform",
+                                                          "search_company"]),
+                "location": self.from_entity(entity="location",
+                                             intent=["inform",
+                                                     "search_company"])}
+
+    def submit(self,
+               dispatcher: CollectingDispatcher,
+               tracker: Tracker,
+               domain: Dict[Text, Any]
+               ) -> List[Dict]:
+        """Once required slots are filled, print buttons for found facilities"""
+
+        location = tracker.get_slot('location')
+        company = tracker.get_slot('company')
+
+        results = _find_company(location, company)
+        button_name = _resolve_name(COMPANIES, company)
+        if len(results) == 0:
+            dispatcher.utter_message(
+                "Sorry, we could not find a {} in {}.".format(button_name,
+                                                              location.title()))
+            return []
+
+
+        buttons = []
+        # limit number of results to 3 for clear presentation purposes
+        for r in results[:3]:
+            if company == COMPANIES["BERLIN"]["resource"]:
+                company_id = r.get("id")
+                name = r["name"]
+            elif company == COMPANIES["Berlin Hbf"]["resource"]:
+                company_id = r["id"]
+                name = r["name"]
+            else:
+                company_id = r["id"]
+                name = r["name"]
+
+            payload = "/inform{\"company_id\":\"" + company_id + "\"}"
+            buttons.append(
+                {"title": "{}".format(name.title()), "payload": payload})
+
+        if len(buttons) == 1:
+            message = "Here is a {} near you:".format(button_name)
+        else:
+            if button_name == "test":
+                button_name = "tests"
+            message = "Here are {} {}s near you:".format(len(buttons),
+                                                         button_name)
+
+        # TODO: update rasa core version for configurable `button_type`
+        dispatcher.utter_button_message(message, buttons)
+
+        return []
